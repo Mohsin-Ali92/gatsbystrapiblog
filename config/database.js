31a153776cbdbb0bc9d346c51533f0e2acbecb4d
Mohsin-Ali92 <mohsin93790@gmail.com>
2021-05-15 19:39:24 +0530

    update config

--- config/database.js
+++ config/database.js
@@ -1,36 +1,15 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         host: env("DATABASE_HOST"),
-//         srv: env.bool("DATABASE_SRV", true),
-//         port: env.int("DATABASE_PORT", 27017),
-//         database: env("DATABASE_NAME"),
-//         username: env("DATABASE_USERNAME"),
-//         password: env("DATABASE_PASSWORD"),
-//       },
-//       options: {
-//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-//         ssl: env.bool("DATABASE_SSL", true),
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.4nxe8.mongodb.net"),
+        host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "cms"),
-        username: env("DATABASE_USERNAME", "Mohsin"),
-        password: env("DATABASE_PASSWORD", "Mohsinali@92"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
